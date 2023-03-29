7e018184d895cd962111e8b0c6c5bc584d0a369f
InnocentEdem <80456476+Quessi@users.noreply.github.com>
2023-03-29 21:34:51 +0000

    Refactor caching
    Update cache key to differentiate similar city names.

--- dist/controllers/index.js
+++ dist/controllers/index.js
@@ -22,23 +22,71 @@ router.get("/coordinates", (req, res) => __awaiter(void 0, void 0, void 0, funct
     const latitude = req.query.lat;
     const longitude = req.query.lon;
     const data = { latitude, longitude };
+    //confirm that latitude and longitude are valid,reject request if not
     if (!(data.latitude && data.longitude)) {
         return res.status(400).json({ message: "Invalid Request Location" });
     }
-    const result = yield (0, fetchWeatherData_1.default)(data);
-    res.status(200).json({ message: "success", data: result });
+    //construct redis key from latitude and longitude
+    const redisKey = latitude.toString().concat(longitude.toString());
+    //check redis cache for data,send response if data
+    try {
+        const redisData = yield redisClient_1.default.get(redisKey);
+        if (!!redisData) {
+            return res
+                .status(200)
+                .json({
+                message: "success",
+                fromCache: true,
+                data: JSON.parse(redisData),
+            });
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+    //fetch data from weather api
+    let result;
+    try {
+        result = yield (0, fetchWeatherData_1.default)(data);
+    }
+    catch (error) {
+        return res.status(502).json({ message: "server network error", code: 502 });
+    }
+    //save data to redis cache and fulfill client request
+    try {
+        yield redisClient_1.default.set(redisKey, JSON.stringify(result));
+        yield redisClient_1.default.expire(redisKey, 6 * 60 * 60);
+    }
+    catch (error) {
+        console.log(error);
+    }
+    return res
+        .status(200)
+        .json({ message: "success", fromCache: "false", data: result });
 }));
 router.get("/city", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     let fromCache = false;
     let results;
     const cityName = req.query.name.toLowerCase();
     let cacheResults;
+    let citiData;
+    let redisCityKey;
+    //Search JSON for city data, reject reques if not found
     try {
-        cacheResults = yield redisClient_1.default.get(cityName);
+        citiData = (yield (0, getCityData_1.getCityData)(cityName));
+    }
+    catch (error) {
+        return res.status(404).json({ message: "Unknown City" });
+    }
+    //Check redis cache for city data
+    try {
+        redisCityKey = citiData.country.concat(citiData.name).toLocaleLowerCase();
+        cacheResults = yield redisClient_1.default.get(redisCityKey);
     }
     catch (error) {
         console.log(error);
     }
+    //If data found in cache, request succeeds
     if (!!cacheResults) {
         fromCache = true;
         results = JSON.parse(cacheResults);
@@ -46,18 +94,19 @@ router.get("/city", (req, res) => __awaiter(void 0, void 0, void 0, function* ()
             .status(200)
             .json({ message: "success", fromCache, data: results });
     }
-    let citiData;
+    //Request not found in cache, fetch data from API
     try {
-        citiData = (yield (0, getCityData_1.getCityData)(cityName));
+        const data = { latitude: +(citiData === null || citiData === void 0 ? void 0 : citiData.lat), longitude: +(citiData === null || citiData === void 0 ? void 0 : citiData.lng) };
+        results = yield (0, fetchWeatherData_1.default)(data);
     }
     catch (error) {
-        return res.status(404).json({ message: "Unknown City" });
+        return res.status(502).json({ message: "Server network error", code: 502 });
     }
-    const data = { latitude: +(citiData === null || citiData === void 0 ? void 0 : citiData.lat), longitude: +(citiData === null || citiData === void 0 ? void 0 : citiData.lng) };
-    results = yield (0, fetchWeatherData_1.default)(data);
+    //Don't forget to update cache with API results before response is returned
     try {
-        yield redisClient_1.default.set(cityName, JSON.stringify(results));
-        yield redisClient_1.default.expire(cityName, 12 * 60 * 60);
+        redisCityKey = citiData.country.concat(citiData.name).toLocaleLowerCase();
+        yield redisClient_1.default.set(redisCityKey, JSON.stringify(results));
+        yield redisClient_1.default.expire(redisCityKey, 6 * 60 * 60);
     }
     catch (error) {
         console.log(error);
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -7,28 +7,78 @@ import { ICityData } from "../types";
 
 const router = express.Router();
 
-router.get("/coordinates", async(req, res) => {
+router.get("/coordinates", async (req, res) => {
   const latitude = req.query.lat as unknown as number;
   const longitude = req.query.lon as unknown as number;
   const data = { latitude, longitude };
+
+  //confirm that latitude and longitude are valid,reject request if not
   if (!(data.latitude && data.longitude)) {
     return res.status(400).json({ message: "Invalid Request Location" });
   }
-  const result = await fetchData(data);
-  res.status(200).json({ message: "success", data: result });
+  //construct redis key from latitude and longitude
+  const redisKey = latitude.toString().concat(longitude.toString());
+
+  //check redis cache for data,send response if data
+  try {
+    const redisData = await redisClient.get(redisKey);
+    if (!!redisData) {
+      return res
+        .status(200)
+        .json({
+          message: "success",
+          fromCache: true,
+          data: JSON.parse(redisData),
+        });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  //fetch data from weather api
+  let result: any;
+  try {
+    result = await fetchData(data);
+  } catch (error) {
+    return res.status(502).json({ message: "server network error", code: 502 });
+  }
+
+  //save data to redis cache and fulfill client request
+  try {
+    await redisClient.set(redisKey, JSON.stringify(result));
+    await redisClient.expire(redisKey, 6 * 60 * 60);
+  } catch (error) {
+    console.log(error);
+  }
+  return res
+    .status(200)
+    .json({ message: "success", fromCache: "false", data: result });
 });
 
 router.get("/city", async (req, res) => {
   let fromCache = false;
   let results;
   const cityName = (req.query.name as string).toLowerCase();
-  let cacheResults
-  try {
-    cacheResults = await redisClient.get(cityName);
+  let cacheResults;
+  let citiData: ICityData;
+  let redisCityKey: string;
 
+  //Search JSON for city data, reject reques if not found
+  try {
+    citiData = (await getCityData(cityName)) as ICityData;
   } catch (error) {
-    console.log(error)
+    return res.status(404).json({ message: "Unknown City" });
   }
+
+  //Check redis cache for city data
+  try {
+    redisCityKey = citiData.country.concat(citiData.name).toLocaleLowerCase();
+    cacheResults = await redisClient.get(redisCityKey);
+  } catch (error) {
+    console.log(error);
+  }
+
+  //If data found in cache, request succeeds
   if (!!cacheResults) {
     fromCache = true;
     results = JSON.parse(cacheResults);
@@ -36,19 +86,22 @@ router.get("/city", async (req, res) => {
       .status(200)
       .json({ message: "success", fromCache, data: results });
   }
-  let citiData: ICityData;
+
+  //Request not found in cache, fetch data from API
   try {
-    citiData = (await getCityData(cityName)) as ICityData;
+    const data = { latitude: +citiData?.lat, longitude: +citiData?.lng };
+    results = await fetchData(data);
   } catch (error) {
-    return res.status(404).json({ message: "Unknown City" });
+    return res.status(502).json({ message: "Server network error", code: 502 });
   }
-  const data = { latitude: +citiData?.lat, longitude: +citiData?.lng };
-  results = await fetchData(data);
+
+  //Don't forget to update cache with API results before response is returned
   try {
-    await redisClient.set(cityName, JSON.stringify(results));
-    await redisClient.expire(cityName, 12 * 60 * 60);
+    redisCityKey = citiData.country.concat(citiData.name).toLocaleLowerCase();
+    await redisClient.set(redisCityKey, JSON.stringify(results));
+    await redisClient.expire(redisCityKey, 6 * 60 * 60);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
   return res.status(200).json({ message: "success", fromCache, data: results });
 });
